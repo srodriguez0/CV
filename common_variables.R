7fef61fb2dcad5c442fb525d1cd8351b2bafdcd4
ifeomaozo <ifeoma.ozodiegwu@northwestern.edu>
2021-01-11 08:19:44 -0600

    Added html_to_pdf script

--- common_variables.R
+++ common_variables.R
@@ -15,4 +15,4 @@ skills <- tribble(
 )
 
 
-pagedown::chrome_print("C:/Users/ido0493/Documents/CV/Ifeoma Ozodiegwu's Resume_.html")
+
---
+++ html_to_pdf.R
@@ -0,0 +1,1 @@
+pagedown::chrome_print("C:/Users/ido0493/Documents/CV/Ifeoma Ozodiegwu's Resume_.html")
